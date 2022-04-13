5d60fe47a7c285e3437b7710e0dfdc5cac4d33be
ms785314 <ms785314@gmail.com>
2022-04-13 20:13:01 +0600

    services added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './Pages/Home/Home/Home';
+import Footer from './Pages/Shared/Footer/Footer';
+import Header from './Pages/Shared/Header/Header';
 
 function App() {
   return (
     <div className="App">
-      
+      <Header></Header>
+      <Routes>
+       <Route path='/home' element={<Home></Home>}></Route>
+      </Routes>
+      <Footer></Footer>
     </div>
   );
 }
